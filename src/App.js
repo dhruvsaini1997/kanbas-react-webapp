4184739c5772b61b20a6645770e941798ee02c2f
dhruv saini <dhruvsaini1997@gmail.com>
2023-12-05 13:52:36 -0500

    a6

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           <Route path="/Kanbas/*" element={<Kanbas />} />
           <Route path="/Labs/a4/*" element={<Assignment4/>}/>
           <Route path="/Labs/a5/*" element={<Assignment5/>}/>
-          <Route path="/project/*" element={<Project />} />
+          <Route path="/project/*" element={<Project/>} />
           {/* <Route path="/Lectures" element={<StateManagement />} /> */}
         </Routes>
         {/* {screen === "Hello" && <HelloWorld />}
